7ca89ca65739f71d2f5e3e975fe77fccb75cb300
Alexia Sander <sander.alexia@gmail.com>
2023-08-10 19:54:00 +0900

    controller updated with new, create and private joy_params

--- app/controllers/joys_controller.rb
+++ app/controllers/joys_controller.rb
@@ -1,7 +1,28 @@
 class JoysController < ApplicationController
 
+  def new
+    @joy = Joy.new
+  end
+
+  def create
+    @joy = Joy.new(joy_params)
+    @joy.user = current_user
+    if @joy.save
+      redirect_to new_joy_path
+    else
+      flash[:alert] = 'Something went wrong.'
+      render :new
+    end
+  end
+
   def index
     @joys = current_user.joys.all
+  end
+
+  private
+
+  def joy_params
+    params.require(:joy).permit(:description, :rating)
   end
 
 end
