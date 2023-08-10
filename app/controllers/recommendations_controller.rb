bf3c2d48bb385b269435d3ee00bae9ec7b345d56
Saki Ebihara <sebihara729@gmail.com>
2023-08-10 21:23:10 +0900

    got rid of game id validation in joy model

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,15 @@
 class RecommendationsController < ApplicationController
+  def show
+
+  end
+
+  private
+
+  def set_recommendations
+    @recommendation = Recommendation.find(params[:id])
+  end
+
+  def recommendation_params
+    params.require(:recommendation).permit(:activity, :rating, :genre)
+  end
 end
