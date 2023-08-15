b63e824cb36f642dfa1649cb1faa5181e502a51c
Alexia Sander <sander.alexia@gmail.com>
2023-08-15 20:41:39 +0900

    pg search added in reco model

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,4 +2,11 @@ class Recommendation < ApplicationRecord
   belongs_to :user
 
   validates :activity, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_activity,
+    against: [ :activity ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
