3d245f31a40bf1dc9e2963e67a1fb5c0ccda8dae
Saki Ebihara <sebihara729@gmail.com>
2023-08-19 16:25:03 +0900

    gamecard job and stable diffusion service class

--- app/jobs/gamecard_job.rb
+++ app/jobs/gamecard_job.rb
@@ -3,15 +3,15 @@ class GamecardJob < ApplicationJob
 
   def perform(joy)
     game =
-      if Game.last.gamecards.count == 5
+      if Game.where(user_id: joy.user_id).last.gamecards.count == 5
         # send notification that new game is available?
         Game.new(user_id: joy.user_id)
       else
-        Game.last
+        Game.where(user_id: joy.user_id).last
       end
     card = Gamecard.new(joy_id: joy.id, game_id: game.id)
-    # image_url = StableDiffusionService.new(joy.description).call
-    # card.image_url = image_url
+    image_url = StableDiffusionService.new(joy.description).call
+    card.image_url = image_url
     card.save
   end
 end
--- app/services/stablediffusion_service.rb
+++ app/services/stablediffusion_service.rb
@@ -1,15 +1,34 @@
 # app/services/stablediffusion_service.rb
 
-
 class StablediffusionService
-  attr_reader :client, :prompt
-
   def initialize(prompt)
-    @client = OpenAI::Client.new
     @prompt = prompt
   end
 
   def call
-
+    url = "https://stablediffusionapi.com/api/v3/text2img"
+    options = {
+      body: {
+        "key": ENV["SD_ACCESS_TOKEN"],
+        "prompt": @prompt,
+        "negative_prompt": nil,
+        "width": "512",
+        "height": "512",
+        "samples": "1",
+        "num_inference_steps": "20",
+        "seed": nil,
+        "guidance_scale": 7.5,
+        "safety_checker": "yes",
+        "multi_lingual": "no",
+        "panorama": "no",
+        "self_attention": "no",
+        "upscale": "no",
+        "embeddings_model": nil,
+        "webhook": nil,
+        "track_id": nil
+      }
+    }
+    response = HTTParty.post(url, options)
+    response["output"].first
   end
 end
