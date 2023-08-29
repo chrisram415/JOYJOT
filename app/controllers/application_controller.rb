451b80f6d89bea336c41cd09dc133d5ccc5af247
Alexia Sander <sander.alexia@gmail.com>
2023-08-29 21:41:31 +0900

    added some steps for naked domain and title tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # Controller responsible for managing the application.
+
+# app/controllers/application_controller.rb  def default_url_options   { host: ENV["DOMAIN"] || "localhost:3000" } end
+
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :initialize_session
