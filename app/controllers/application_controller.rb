60d80467395d6384f745bd81e18edfc58ab13623
Alexia Sander <sander.alexia@gmail.com>
2023-08-31 22:10:18 +0900

    step 2 3 and 4 should be done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :initialize_session
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def initialize_session
     session[:weekly_task_executed] ||= false
     session[:button_triggered] ||= false
   end
+
 end
