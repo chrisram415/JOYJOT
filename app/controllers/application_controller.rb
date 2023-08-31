72faf663ba6454226c8e02981d58adf8b5b264eb
Alexia Sander <sander.alexia@gmail.com>
2023-08-31 19:16:47 +0900

    cursive replaced by sans-serif

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 # Controller responsible for managing the application.
 
-# app/controllers/application_controller.rb  def default_url_options   { host: ENV["DOMAIN"] || "localhost:3000" } end
-
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :initialize_session
